2f425e34c8d99718595134a8c820a360570290cc
jeremy_plott <plott.j@pg.com>
2020-02-13 11:52:57 -0500

    Add project files.

---
+++ LCRemoveDuplicatesFromSortedArray/LC_syntax_commented.cs
@@ -0,0 +1,33 @@
+﻿This is pretty straightforward, although a little weird with what the problem wants as a return coupled with the modified in-place requirement.
+
+```
+"public class Solution {
+
+	public int RemoveDuplicates(int[] nums)
+{
+
+	//we need to track the number of unique digits because it wants an integer return of that count.
+	//this will also be the index of where we write our new numbers to the start of the array.
+	var uniqueNums = 0;
+
+	//we will iterate through the array to check every number
+	for (int num = 0; num < nums.Length; num++)
+	{
+		//since this array is sorted its very easy to skip duplicates.
+		//we use length - 1 and < because we will get out of range exceptions with the num + 1 index check.
+		//if the current number = the next number, skip it.
+		while (num < nums.Length - 1 && nums[num] == nums[num + 1]) num++;
+
+		//the diplicate numbers have been skipped, so the current number must be unique.
+		//add it to the start of the array
+		nums[uniqueNums] = nums[num];
+
+		//now increment our counter / unique number writer
+		uniqueNums++;
+	}
+
+	//return the count of unique numbers. The site will check if the array has been modified.
+	return uniqueNums;
+}
+}
+```
---
+++ LCRemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LCRemoveDuplicatesFromSortedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5 };
+
+            RemoveDuplicates(nums);
+
+            foreach (var num in nums) { Console.WriteLine(num); }
+
+            int[] RemoveDuplicates(int[] nums)
+            {
+                var uniqueNums = 0;
+
+                for (int num = 0; num < nums.Length; num++)
+                {
+                    while (num < nums.Length - 1 && nums[num] == nums[num + 1]) num++;                    
+
+                    nums[uniqueNums] = nums[num];
+
+                    uniqueNums++;
+                }
+
+                return nums;
+            }
+        }
+    }
+}
